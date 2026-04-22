d903d9188e835ebddaff79dc7d0c7c0143cb9bf3
okta <fachrudinokta@gmail.com>
2026-04-22 10:31:20 +0700

    hehe

--- test2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_1 = require("./lib/prisma");
-prisma_1.prisma.user.findFirst().then(console.log).catch(console.error);
